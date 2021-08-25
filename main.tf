4d842c81ecaad5d0255f76ad4baaf75cf318edd1
KshamaG <kgurudut@in.ibm.com>
2021-08-25 13:14:25 +0530

    add output resource_cloud block in main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,13 @@ resource "null_resource" "sleep" {
   }
 }
 
+output "resource_cloud" {
+  value = {
+    "is_resource" = "true"
+    "resource_controller_url" = ""
+    "resource_icon_url" = "https://globalcatalog.test.cloud.ibm.com/api/v1/1082e7d2-5e2f-0a11-a3bc-f88a8e1931fc/artifacts/cache/8c6189a76c7d346f6e807aaffb3bf643-public/terraform.svg"
+    "resource_id" = "42381608-4af0-f4dc-363d-a3a79c17d927"
+    "resource_name" = "teste-tadeu25"
+  }
+}
+
